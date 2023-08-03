76643a4abbb0bdc8295048290834cee9d1c2434f
adiyat-shihab <mohammadshihab104@gmail.com>
2023-08-04 02:42:23 +0600

    all of my projects  from July to August

---
+++ doctor-portfolio/grade.js
@@ -0,0 +1,24 @@
+const mathmathmetics = 75.25;
+const biology = 65;
+const chemistry = 80;
+const physics = 35.45;
+var bangla = 99.5;
+
+totalmarks = mathmathmetics + biology + chemistry + physics + bangla;
+average = totalmarks / 5;
+result = average.toFixed(2);
+console.log(result);
+
+const sarahGivenNumber = 119;
+resultNumber = sarahGivenNumber % 5;
+console.log(resultNumber);
+let fruits = ["apple", "banana", "cherry", "orange"];
+fruits[1] = "mango";
+fruits.pop();
+fruits.push("watermelon");
+console.log(fruits);
+
+var i = 5;
+for (; i < 5; i++) {
+  console.log(i);
+}
